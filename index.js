476e9dab757547cdc92fc74757ee9d584f179f5b
Don <donducdo@gmail.com>
2022-07-17 00:25:57 -0700

    Finished Design

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ let skies = document.querySelector(".skies")
 let humidity = document.querySelector(".humidity")
 let precipitation = document.querySelector(".precipitation")
 
+let skiesValue = document.querySelector(".skies-value")
+let humidityValue = document.querySelector(".humidity-value")
+
+
 // Sunrise and Sunset Time
 
 let sunrise = document.querySelector(".sunrise-time")
@@ -37,6 +41,33 @@ function toCelsius(fahren){
     return ((fahren - 32) * .5556).toFixed(1)
 }
 
+// Weather 
+
+let weatherConditions = 
+["Clear", "Clouds", "Rain", "Drizzle", 
+"Thunderstorm", "Snow", "Mist", "Smoke", "Haze", "Dust", 
+"Fog", "Sand", "Ash", "Squall", "Tornado"]
+
+// <i class="fa-solid fa-cloud fa-7x"></i>
+//         <!-- <i class="fa-solid fa-sun"></i> -->
+//         <!-- <i class="fa-solid fa-cloud-rain"></i> -->
+//         <!-- <i class="fa-solid fa-cloud-snow"></i>  -->
+//         <!-- <i class="fa-solid fa-cloud-bolt"></i> -->
+
+// Time Conversion
+
+function convertTime(unix){
+    let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+    },
+    intlDate = new Intl.DateTimeFormat(undefined, options);
+    
+    let timeStamp = unix;
+    let time = intlDate.format( new Date( 1000 * timeStamp));
+    return time
+}
+
 
 // Fetch Request
 
@@ -47,25 +78,15 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=San Jose&appid=e79ab9c1
         let celsius = toCelsius(data.main.temp);
         temperature.textContent = data.main.temp.toFixed(1) + "°F" + " or " + celsius + "°C";
         place.textContent = data.name
-        // skies.textContent = "Skies " + data.weather[0].main
-        // humidity.textContent = "Humidity " + data.main.humidity + "%"
-        // precipitation.textContent = precipitation.value
-        console.log(data.weather[0].main)
+        skiesValue.textContent = data.weather[0].main
+        humidityValue.textContent = data.main.humidity + "%"
+        sunrise.textContent = convertTime(data.sys.sunrise)
+        sunset.textContent = convertTime(data.sys.sunset)
+        console.log(data)
     })
     .catch(console.error())
 
+weatherButton.addEventListener("click", function(){
 
+})
 
-// fetch("e79ab9c18b89145630220d4377fa6219", {
-//     method: "GET",
-//     body: JSON.stringify({
-//         title: "Hello",
-//         completed: true
-//     }),
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(console.error)
